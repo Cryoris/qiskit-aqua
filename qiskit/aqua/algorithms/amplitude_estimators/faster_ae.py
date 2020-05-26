b9147db8aa77476bf2aa70975944d4f8d2fb4475
Cryoris <jules.gacon@googlemail.com>
2020-05-26 22:31:48 +0200

    update documentation

--- qiskit/aqua/algorithms/amplitude_estimators/faster_ae.py
+++ qiskit/aqua/algorithms/amplitude_estimators/faster_ae.py
@@ -14,48 +14,86 @@
 
 """Faster Amplitude Estimation."""
 
+from typing import Optional, Union, Tuple, List
 import numpy as np
 
 from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.providers import BaseBackend
+from qiskit.aqua import QuantumInstance
 from qiskit.aqua.aqua_error import AquaError
 from qiskit.aqua.utils import CircuitFactory
 from .ae_algorithm import AmplitudeEstimationAlgorithm
 from .q_factory import QFactory
 
-# pylint:disable=invalid-name
-
 
 class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
-    """
+    """The Faster Amplitude Estimation algorithm.
+
+    The Faster Amplitude Estimation (FAE) [1] algorithm is a variant of Quantum Amplitude
+    Estimation (QAE), where the Quantum Phase Estimation (QPE) by an iterative Grover search,
+    similar to [2].
+
+    Due to the iterative version of the QPE, this algorithm does not require extra ancilla
+    qubits, as the originally proposed QAE [3] and the resulting circuits are less complex.
 
     References:
 
-        [1]: `arXiv:2002.02417 <https://arxiv.org/pdf/2003.02417.pdf>`_
+        [1]: K. Nakaji. Faster Amplitude Estimation, 2020;
+            `arXiv:2002.02417 <https://arxiv.org/pdf/2003.02417.pdf>`_
+        [2]: D. Grinko et al. Iterative Amplitude Estimation, 2019;
+            `arXiv:1912.05559 <http://arxiv.org/abs/1912.05559>`_
+        [3]: G. Brassard et al. Quantum Amplitude Amplification and Estimation, 2000;
+            `arXiv:quant-ph/0005055 <http://arxiv.org/abs/quant-ph/0005055>`_
+
     """
 
-    def __init__(self, N, delta, maxiter, a_factory=None, q_factory=None, x_factory=None,
-                 i_objective=None, quantum_instance=None):
+    def __init__(self, N: Tuple[int, int], delta: float, maxiter: int,
+                 a_factory: Optional[CircuitFactory] = None,
+                 q_factory: Optional[CircuitFactory] = None,
+                 x_factory: Optional[CircuitFactory] = None,
+                 i_objective: Optional[List[int]] = None,
+                 quantum_instance: Optional[Union[QuantumInstance, BaseBackend]] = None) -> None:
+        r"""
+        Args:
+            N: The number of shots for the first and second stage as tuple.
+            delta: The probability that the true value is outside of the final confidence interval.
+            maxiter: The number of iterations, the maximal power of Q is `2 ** (maxiter - 1)`.
+            a_factory: The A operator constructing the amplitudes.
+            q_factory: The Q, or Grover, operator.
+            x_factory: The X operator, defined as :math:`A \otimes R_y(\sin^{-1}(1/4))`.
+            i_objective: The objective qubits, defaults to the last two qubits.
+            quantum_instance: The quantum instance or backend to run the circuits.
+        """
         super().__init__(a_factory, q_factory, i_objective, quantum_instance)
-        self._N = N
+        self._shots = N
         self._delta = delta
         self._maxiter = maxiter
         self._num_oracle_calls = 0
         self._x_factory = x_factory
 
     @property
-    def x_factory(self):
-        """X = A x R"""
+    def x_factory(self) -> Optional[CircuitFactory]:
+        r"""The X operator, defined as :math:`A \otimes R_y(\sin^{-1}(1/4))`.
+
+        Returns:
+            The X operator, computed based on the current A operator, or None if the A operator
+            is not defined.
+        """
         if self._x_factory is not None:
             return self._x_factory
 
         if self._a_factory is not None:
-            return XFactory(self._a_factory)
+            return XFactory(self._a_factory, scaling_factor=0.25)
 
         return None
 
     @property
-    def q_factory(self):
-        """Special Q operator for Faster AE"""
+    def q_factory(self) -> Optional[CircuitFactory]:
+        r"""The Q operator.
+
+        Returns:
+            The current Q operator, based on the A operator, of None if the A operator is not set.
+        """
         if self._q_factory is not None:
             return self._q_factory
 
@@ -66,8 +104,12 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
         return None
 
     @property
-    def i_objective(self):
-        """Objective qubits"""
+    def i_objective(self) -> Optional[List[int]]:
+        """The objective qubits, if all these qubits are in state |1> the state is marked as good.
+
+        Returns:
+            The objective qubits, or None if not set and the A operator is not set.
+        """
         if self._i_objective is not None:
             return self._i_objective
 
@@ -87,11 +129,11 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
             self._quantum_instance.run_config.shots = shots
             counts = self._quantum_instance.execute(circuit).get_counts()
             self._num_oracle_calls += (2 * k + 1) * shots
-            cos_estimate = 1 - 2 * counts.get('11', 0) / shots
+            cos_estimate = 1 - 2 * counts.get('1' * len(self.i_objective), 0) / shots
 
         return cos_estimate
 
-    def _churnoff(self, cos, shots):
+    def _chernoff(self, cos, shots):
         width = np.sqrt(np.log(2 / self._delta) * 12 / shots)
         confint = [np.maximum(-1, cos - width), np.minimum(1, cos + width)]
         return confint
@@ -126,8 +168,8 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
 
         # add classical register if needed
         if measurement:
-            c = ClassicalRegister(2)
-            # c = ClassicalRegister(1)
+            num_objective = len(self.i_objective)
+            c = ClassicalRegister(num_objective)
             circuit.add_register(c)
 
         # add A operator
@@ -142,13 +184,9 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
             # real hardware can currently not handle operations after measurements, which might
             # happen if the circuit gets transpiled, hence we're adding a safeguard-barrier
             circuit.barrier()
-            circuit.measure([q[self.i_objective[0]], q[self.i_objective[1]]], [c[0], c[1]])
-            # circuit.measure(q[self.i_objective[0]], c[0])
+            circuit.measure([q[i] for i in self.i_objective], [c[i] for i in range(num_objective)])
 
         return circuit
-
-    def value_to_estimation(self, value):
-        return self._a_factory.value_to_estimation(value)
 
     def _run(self):
         self._num_oracle_calls = 0
@@ -163,19 +201,19 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
             num_steps += 1
             if first_stage:
                 num_first_stage_steps += 1
-                c = self._cos_estimate(2**(j - 1), self._N[0])
-                ci = self._churnoff(c, self._N[0])
-                theta_ci = [np.arccos(x) / (2 ** (j + 1) + 2) for x in ci[::-1]]
+                c = self._cos_estimate(2**(j - 1), self._shots[0])
+                chernoff_ci = self._chernoff(c, self._shots[0])
+                theta_ci = [np.arccos(x) / (2 ** (j + 1) + 2) for x in chernoff_ci[::-1]]
 
                 if 2 ** (j + 1) * theta_ci[1] >= 3 * np.pi / 8 and j < self._maxiter:
-                    j0 = j
+                    j_0 = j
                     v = 2 ** j * np.sum(theta_ci)
                     first_stage = False
             else:
-                c = self._cos_estimate(2**(j - 1), self._N[1])
-                c2 = self._cos_estimate(2 ** (j - 1) + 2 ** (j0 - 1), self._N[1])
-                s = (c * np.cos(v) - c2) / np.sin(v)
-                rho = np.arctan2(s, c)
+                cos = self._cos_estimate(2**(j - 1), self._shots[1])
+                cos_2 = self._cos_estimate(2 ** (j - 1) + 2 ** (j_0 - 1), self._shots[1])
+                sin = (c * np.cos(v) - cos_2) / np.sin(v)
+                rho = np.arctan2(sin, cos)
                 n = int(((2 ** (j + 1) + 2) * theta_ci[1] - rho) / (2 * np.pi))
 
                 theta_ci = [(2 * np.pi * n + rho + sign * np.pi / 2) / (2 ** (j + 1) + 2)
@@ -191,8 +229,8 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
             'theta_ci': theta_ci,
             'value': value,
             'value_ci': value_ci,
-            'estimate': self.value_to_estimation(value),
-            'confint': [self.value_to_estimation(x) for x in value_ci],
+            'estimate': self._a_factory.value_to_estimation(value),
+            'confint': [self._a_factory.value_to_estimation(x) for x in value_ci],
             'num_oracle_calls': self._num_oracle_calls,
             'theta_cis': theta_cis,
             'num_steps': num_steps,
@@ -203,26 +241,37 @@ class FasterAmplitudeEstimation(AmplitudeEstimationAlgorithm):
 
 
 class XFactory(CircuitFactory):
-    """X = A x R"""
+    """The X operator, rescales the amplitude of the A operator using an ancilla.
 
-    def __init__(self, a_factory):
+    The A operator in Quantum Amplitude Estimation performs the mapping
+
+    .. math::
+
+        todo
+
+    Somce QAE variants, as Faster Amplitude Estimation and QAE Simplified, require the amplitude
+    :math:`a` to be smaller than a certain threshold. This rescaling is achieved with with a Pauli-Y
+    rotation on an ancilla qubit with an according rotation angle.
+    """
+
+    def __init__(self, a_factory: CircuitFactory, scaling_factor: float) -> None:
+        """
+        Args:
+            a_factory: The A operator.
+            scaling_factor: The rescaling factor, must be smaller than 1.
+        """
         super().__init__(a_factory.num_target_qubits + 1)
         self.a_factory = a_factory
+        self.scaling_factor = scaling_factor
 
     # pylint: disable=unused-argument
     def build(self, qc, q, q_ancillas=None, params=None):
         self.a_factory.build(qc, q, q_ancillas)
-        qc.ry(2 * np.arcsin(0.25), q[self.num_target_qubits - 1])
+        qc.ry(2 * np.arcsin(self.scaling_factor), q[self.num_target_qubits - 1])
 
 
 class S11Factory(CircuitFactory):
     """Flip if the last two qubits are in |11>"""
-
-    def required_ancillas(self):
-        return 0
-
-    def required_ancillas_controlled(self):
-        return 0
 
     # pylint:disable=unused-argument
     def build(self, qc, q, q_ancillas=None, params=None):
